bae02f77a49080a3fefa3d95e58189790609cc3f
viettbq2002 <thaibinhquocviet2002@gmail.com>
2025-08-05 14:48:38 +0700

    feat: add bundle file

---
+++ dist/bundle.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  function add(a, b) {
+    return a + b;
+  }
+  function greet(name) {
+    return `Hello, ${name}!`;
+  }
+
+  console.log(add(2, 3)); 
+  console.log(greet('Viet Thai'));
+
+})();
