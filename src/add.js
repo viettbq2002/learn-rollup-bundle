95a15feaf3113901422ccee0d3582743b6e07499
viettbq2002 <thaibinhquocviet2002@gmail.com>
2025-08-05 14:46:59 +0700

    first commit

---
+++ src/add.js
@@ -0,0 +1,6 @@
+export function add(a, b) {
+  return a + b;
+}
+export function greet(name) {
+  return `Hello, ${name}!`;
+}
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import  { add, greet } from './add.js';
+
+console.log(add(2, 3)); 
+console.log(greet('Viet Thai')); 
